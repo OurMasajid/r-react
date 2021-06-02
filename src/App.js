1125c59d2e1d392852a7b9b3eeaa563abdb566d4
Faizan <akaban01@gmail.com>
2021-06-01 19:04:51 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+//custom components
+import PrayerDailyTv1 from './code/prayer/daily-tv1'
+import Home from './code/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/prayer-tv1">
+          <PrayerDailyTv1 />
+        </Route>
+        {/* <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route> */}
+      </Switch>
+    </Router>
   );
 }
 
